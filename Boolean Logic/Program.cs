38f40f252bc5e15291a1789b70cc1249e9238434
BBun-bun <1anthony.mds@gmail.com>
2023-08-16 20:35:23 +0100

    mutliple assignemnts uploaded

---
+++ Boolean Logic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Boolean_Logic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Application"); //prints Car Insurance Application
+            Console.WriteLine("What age are you?"); //prints What age are you?
+            int userAge = Convert.ToInt32(Console.ReadLine()); //sets an interger from the users input age 
+            Console.WriteLine("Have you ever had a DUI? please answer with Yes or No"); //prints Have you ever had a DUI? please answer with Yes or No
+            String duiAnswer = Console.ReadLine().ToLower(); //sets the users input to the question above as a string and makes it all lower text
+            bool dui = duiAnswer == "yes"; //sets the bool to true if the users answer from the set string is 'yes'
+            Console.WriteLine("how many speeding tickets do you have?"); //prints how many speeding tickets do you have?
+            int speedingTickets = Convert.ToInt32(Console.ReadLine()); //sets an interger from the user input
+            Console.WriteLine("Do you qualify for insurance?"); //prints Do you qualify for insurance?
+            bool qualified = (userAge > 15) && !dui && (speedingTickets <= 3); //calculates if a user can get insurance if, their age is greater then 15, they dont have any DUI's, have less than or equal to 3 speeding tickets. sets a bool depending on results
+            Console.WriteLine(qualified); //tells the user if they can get insurance with a True or False
+            Console.ReadLine();// gives the user enough time to read the results
+        }
+    }
+}
---
+++ Branching Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Branching_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below."); //prints Welcome to Package Express. Please follow the instructions below.
+            Console.WriteLine("Please enter your packages weight"); //prints Please enter your packages weight
+            int weight = Convert.ToInt32(Console.ReadLine()); //stores the users weight input as an int
+
+            if (weight > 50) //if the weight is greater then 50 execure this 
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day."); //prints Package too heavy to be shipped via Package Express. Have a good day.
+                Console.ReadLine(); //gives the user enough time to read the console
+            }
+            else //if the weight is less then 50 then execute this
+            {
+                Console.WriteLine("please enter package Width"); //prints please enter package Width
+                int width = Convert.ToInt32(Console.ReadLine()); //stores the users width input as an int
+                Console.WriteLine("please enter package height"); //prints please enter package height
+                int height = Convert.ToInt32(Console.ReadLine()); //stores the users height input as an int
+                Console.WriteLine("Please enter pachage length"); //prints Please enter pachage length
+                int length = Convert.ToInt32(Console.ReadLine()); //stores the users length input as an int
+
+                if (width + height + length > 50) //if the width, height and legnth all added together are greater than 50 execute this code
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express."); //prints Package too big to be shipped via Package Express.
+                    Console.ReadLine(); //gives the user enough time to read the console
+                }
+                else //if the width, height and legnth all added together are less than 50 execute this code
+                {
+                    int quote = Convert.ToInt32(((width * height * length) * weight) / 100); //creates an int that is the width, height, lenth all multiplied by each other. then multiplied by the weight, then divided by 100 to calculate the quote
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + quote); //prints Your estimated total for shipping this package is: $ + the calculated quote
+                    Console.ReadLine(); //gives the user enough time to read the console
+                }
+            }
+        }
+    }
+}
