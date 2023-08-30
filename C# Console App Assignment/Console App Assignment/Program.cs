03d40b3bc9815ae0a153de00a850fe62535142c1
BBun-bun <1anthony.mds@gmail.com>
2023-08-30 11:01:31 +0100

    Console App Assignment Part 1

---
+++ C# Console App Assignment/Console App Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] names = { "ben", "jeff", "steve", "harvey", "dave", "bob", "gwen", "rammus", "tristana" };
+            Console.WriteLine("please write and adjective for a person you like, eg: handsome.");
+            string stringAdjective = Console.ReadLine();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                string[] namesUpdated = { names[i] + " is " + stringAdjective };
+
+                for (int j = 0; j < namesUpdated.Length; j++)
+                {
+                    Console.WriteLine(namesUpdated[j]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
