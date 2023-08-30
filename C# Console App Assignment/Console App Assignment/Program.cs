fdb5384765a93092f5a8a8a953d8b045d1b5526f
BBun-bun <1anthony.mds@gmail.com>
2023-08-30 11:17:10 +0100

    Console App Assignment Part 2

--- C# Console App Assignment/Console App Assignment/Program.cs
+++ C# Console App Assignment/Console App Assignment/Program.cs
@@ -10,20 +10,30 @@ namespace Console_App_Assignment
     {
         static void Main(string[] args)
         {
-            String[] names = { "ben", "jeff", "steve", "harvey", "dave", "bob", "gwen", "rammus", "tristana" };
-            Console.WriteLine("please write and adjective for a person you like, eg: handsome.");
-            string stringAdjective = Console.ReadLine();
+            String[] names = { "ben", "jeff", "steve", "harvey", "dave", "bob", "gwen", "rammus", "tristana" }; //a list of peoples names in an array
+            Console.WriteLine("please write and adjective for a person you like, eg: handsome."); //asks the user to input a string 
+            string stringAdjective = Console.ReadLine(); //stores the inputed string of the user
 
-            for (int i = 0; i < names.Length; i++)
+            for (int i = 0; i < names.Length; i++) //loops through each string in the array
             {
-                string[] namesUpdated = { names[i] + " is " + stringAdjective };
+                string[] namesUpdated = { names[i] + " is " + stringAdjective };//adds the user input string at the end of each string i nthe array
 
-                for (int j = 0; j < namesUpdated.Length; j++)
+                for (int j = 0; j < namesUpdated.Length; j++) //loops through the updated array
                 {
-                    Console.WriteLine(namesUpdated[j]);
+                    Console.WriteLine(namesUpdated[j]); //prints out all strings inthe array 
                 }
             }
-            Console.ReadLine();
+            Console.ReadLine(); //gives the user time to read the outcome/ they have to input a button to move the code along 
+
+            int a = 10; //defines a as an int value of 10
+            int k = 0; //defines k as an int value of 0
+
+            while (k < a && a < 20) //this loop is infinite as k will always be smaller then a, and a will always be getting increased. to fix this we can add an AND operator so that it can only repeat aslong as a is smaller than 20 
+            {
+                a++; //will increase the value of a
+                Console.WriteLine("This will no longer go on forever"); //will print "This will no longer go on forever" everytime the loop executes
+            }
+            Console.ReadLine(); //gives the user time to read the outcome/ they have to input a button to move the code along 
         }
     }
 }
