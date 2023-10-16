5f8171b139d60f8bd9a8fe2d37554cde0a87e6d8
BBun-bun <1anthony.mds@gmail.com>
2023-10-16 16:08:55 +0100

    Simple student DB

---
+++ Mod 11/StudentDatabaseAssignment/StudentDatabase/StudentDatabase/ViewModels/Grade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentDatabase.ViewModels
+{
+    public class Grade
+    {
+        public int GradeID { get; set; }
+        public string GradeName { get; set; }
+    }
+}
---
+++ Mod 11/StudentDatabaseAssignment/StudentDatabase/StudentDatabase/ViewModels/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentDatabase.ViewModels
+{
+    public class Student : Grade
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Grade Grade { get; set; }
+    }
+}
