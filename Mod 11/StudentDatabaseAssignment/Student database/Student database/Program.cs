df1324381f0b2eb31d0df4c7801794a7ca904f5b
BBun-bun <1anthony.mds@gmail.com>
2023-10-16 20:13:53 +0100

    used correct template

---
+++ Mod 11/StudentDatabaseAssignment/Student database/Student database/Program.cs
@@ -0,0 +1,27 @@
+﻿using Student_database.Classes;
+using StudentDatabase.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.Write("Student saved successfully!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
